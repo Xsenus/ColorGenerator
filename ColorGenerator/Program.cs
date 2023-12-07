e4191b0aaab3c7e47a0d7d37d7e36c330fac33c0
XsenusWork <ilel@list.ru>
2023-12-07 16:47:57 +0700

    Добавьте файлы проекта.

---
+++ ColorGenerator/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ColorGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем список шестнадцатеричных цветов
+            var colorList = GenerateColorList();
+
+            // Путь к файлу для сохранения списка цветов
+            string filePath = "color_list.txt";
+
+            // Сохраняем список в файл
+            SaveColorListToFile(colorList, filePath);
+
+            Console.WriteLine($"Список цветов сохранен в файл: {filePath}");
+        }
+
+        static string[] GenerateColorList()
+        {
+            var colorList = new string[16777216]; // 16^6 = 16777216
+
+            for (int i = 0; i < colorList.Length; i++)
+            {
+                // Форматируем числа в шестнадцатеричную строку и добавляем к списку
+                colorList[i] = "#" + i.ToString("X6");
+            }
+
+            return colorList;
+        }
+
+        static void SaveColorListToFile(string[] colorList, string filePath)
+        {
+            // Записываем каждый цвет в файл
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (var color in colorList)
+                {
+                    writer.WriteLine(color);
+                }
+            }
+        }
+    }
+}
